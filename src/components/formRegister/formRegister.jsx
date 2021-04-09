2cf41dbb4895095a3ac5dd47c4b834c7530fa2b6
Franklin Percicotte <frankpercicotte@gmail.com>
2021-04-09 00:48:30 -0300

    feat(src/components/formRegister):working bug!  some wrong with new version reack-hook-form.

--- src/components/formRegister/formRegister.jsx
+++ src/components/formRegister/formRegister.jsx
@@ -1,10 +1,57 @@
+// import { Button} from "antd";
+import {TextField} from "@material-ui/core";
+import "antd/dist/antd.css";
+
 
 const formRegister = () => {
 
   return(
-    <div>
-      Working in Register....
-    </div>
+    <form className="form">
+      <h1> Cadastre-se</h1>
+      <div>
+        <TextField
+          required
+          id="outlined-required"
+          label="Required"
+          defaultValue="primeiro nome"
+          variant="outlined"
+        />
+        <TextField
+          required
+          id="outlined-disabled"
+          label="Required"
+          defaultValue="último nome"
+          variant="outlined"
+        />
+        <TextField
+          required
+          id="date"
+          label="Required"
+          type="date"
+          defaultValue="2021-01-01"
+          variant="outlined"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+        
+        <TextField
+          required
+          id="outlined-password-input"
+          label="Required"
+          type="password"
+          autoComplete="current-password"
+          variant="outlined"
+        />
+        <TextField
+          required
+          id="outlined-email-input"
+          label="Required"
+          defaultValue="email"
+          variant="outlined"
+        />                   
+      </div>
+    </form>
   );
 }
 
