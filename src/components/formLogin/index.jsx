60e8fec28aa10a53edf882792a48710138af20ae
Suellen Priscilla de Camargo <suellen.priscilla.camargo@gmail.com>
2021-04-11 02:45:19 -0300

    feat(src/components/formLogin): add formLogin

--- src/components/formLogin/index.jsx
+++ src/components/formLogin/index.jsx
@@ -3,44 +3,67 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { useUsers } from "../../providers/UserProvider";
-import { Button } from "antd"
-import { Link } from "react-router-dom"
+import { useHistory } from "react-router-dom"
+// import { Button } from "antd";
+// import { Link } from "react-router-dom";
+// import { ContainerDiv } from "./style";
 
-const schema = yup.object().shape({
-	email: yup.string().email("Email inválido").required("Campo obrigatório"),
-	password: yup
-		.string()
-		.min(6, "Mínimo de 6 dígitos")
-		.required("Campo obrigatório e mínimo de 6 dígitos"),
-});
+import {
+    StyledForm,
+    StyledButton, 
+    StyledParErr,
+    StyledInput,
+    StyledH3,
+	StyledSpan,
+    StyledPar,
+  } from "./style";
 
-const FormLogin = () => {
-	const { login } = useUsers();
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm({
-		resolver: yupResolver(schema),
-	});
-	const onSubmit = (data) => {
-		console.log(data);
-		login(data);
-	};
+		const schema = yup.object().shape({
+			email: yup.string().email("Email inválido").required("Campo obrigatório"),
+			password: yup
+				.string()
+				.min(6, "Mínimo de 6 dígitos")
+				.required("Campo obrigatório e mínimo de 6 dígitos")
+		});
+		
+		const FormLogin = () => {
+
+			let history = useHistory()
+
+			const { login } = useUsers();
+			const {
+				register,
+				handleSubmit,
+				formState: { errors },
+			} = useForm({
+				resolver: yupResolver(schema),
+			});
+			const onSubmit = (data) => {
+				console.log(data);
+				login(data);
+			};
+
+		const handleRegister = () => {
+		  history.push("/register");
+		}
+	  
 
 	return (
-		<form>
-			<h1>Seja bem vindo!</h1>
-			<p>insira seus dados e faça o login</p>
-			<input {...register("email")} type="email"/>
-			<p>{errors.email?.message}</p>
+		<StyledForm>
+			<StyledH3>Seja bem vindo!</StyledH3>
+			<StyledPar>insira seus dados e faça o login</StyledPar>
+			<StyledInput required {...register("email")} type="email" placeholder="email" />
+			<StyledParErr>{errors.email?.message}</StyledParErr>
 
-			<input {...register("password")} type="password"/>
-			<p>{errors.password?.message}</p>
+			<StyledInput required {...register("password")} type="password" placeholder="senha"/>
+			<StyledParErr>{errors.password?.message}</StyledParErr>
 
-			<Button onClick={handleSubmit(onSubmit)}>Entrar</Button>
-			<p>ainda não tem conta? <Link to="/register" >cadastra-se aqui!</Link></p>
-		</form>
+			<StyledButton type="submit" onClick={handleSubmit(onSubmit)} value="Entrar"/>
+
+			<StyledPar> ainda não tem conta? <StyledSpan onClick={handleRegister} inputColor="#EF7272"> Entre aqui! </StyledSpan></StyledPar>
+
+		</StyledForm>
 	);
 };
+
 export default FormLogin;
