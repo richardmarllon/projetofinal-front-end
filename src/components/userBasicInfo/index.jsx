b2a87709f2abd7d34ad9f5f214c85467a87a52f0
Graziela <grazi.m.kranz@gmail.com>
2021-04-11 01:25:14 -0300

    feat(src/components/userBasicInfo): add details, button, img

--- src/components/userBasicInfo/index.jsx
+++ src/components/userBasicInfo/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useUsers } from "../../providers/UserProvider";
-import { ContainerUser, NameUser } from "./style";
+import { ContainerUser, NameUser, Div, P, Button, Avatar } from "./style";
 
 const UserBasicInfo = () => {
 	const { loggedUser, user } = useUsers();
@@ -12,7 +12,27 @@ const UserBasicInfo = () => {
 	return (
 		<>
 			<ContainerUser>
+				<Avatar src=" https://picsum.photos/seed/picsum/100/100"></Avatar>
+				<Button>config</Button>
 				<NameUser>{loggedUser.data.firstName}, teste </NameUser>
+				<Div>
+					sangue:
+					<P>{loggedUser.data.bloodType + loggedUser.data.rhFactor}</P>
+				</Div>
+				<Div>
+					<P>alergias:</P>
+					<P>{loggedUser.data.allergies ? "sim" : "nao"}</P>
+				</Div>
+
+				<Div>
+					<P>detalhes:</P>
+					{loggedUser.data.allergies && (
+						<P>alergico a: {loggedUser.data.allergies}</P>
+					)}
+					{loggedUser.data.pregnant && <P>Grávida: Sim</P>}
+				</Div>
+
+				<Button>vacinas</Button>
 			</ContainerUser>
 		</>
 	);
--- src/components/userBasicInfo/style.js
+++ src/components/userBasicInfo/style.js
@@ -3,3 +3,11 @@ import styled from "styled-components";
 export const ContainerUser = styled.div``;
 
 export const NameUser = styled.h1``;
+
+export const Div = styled.div``;
+
+export const P = styled.p``;
+
+export const Button = styled.button``;
+
+export const Avatar = styled.img``;
