f3c7a6f19f6a314679858adea04eb0cc991a33a9
Illian Curvello Ferreira <illiancurve@gmail.com>
2021-04-14 21:04:03 -0300

    feat(src/__test__/components/formRegister.test.js): added the structure to test de formRegister

--- src/__test__/components/formRegister.test.js
+++ src/__test__/components/formRegister.test.js
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import FormLogin from "../../components/formLogin";
+import FormRegister from "../../components/formRegister";
 import React from "react";
 
 const mockedHandleSubmit = jest.fn();
@@ -11,36 +11,39 @@ jest.mock("react-hook-form", () => {
 			formState: {
 				errors: {
 					email: "mockedEmail",
+					firstName: "mockedFirstName",
+					lastName: "mockedLastName",
+					date: "mockedDate",
 					password: "mockedPassowrd",
+					userType: "mockedUserType",
+					cpf: "mockedCpf",
 				},
 			},
 			register: jest.fn(),
 		}),
 	};
 });
-jest.mock("../../providers/UserProvider", () => {
-	return {
-		useUsers: () => ({
-			login: jest.fn(),
-			formState: {
-				errors: {
-					email: "mockedEmail",
-					password: "mockedPassowrd",
-				},
-			},
-		}),
-	};
-});
 
 describe("Should call handleSubmit", () => {
-	test("When submits the form LOGIN", () => {
-		render(<FormLogin />);
+	test("When submits the form REGISTER", () => {
+		render(<FormRegister />);
 
 		const userEmail = screen.getByTestId("userEmailTestId");
 		const passwordInput = screen.getByTestId("passwordTestId");
+		const firstNameInput = screen.getByTestId("firstNameTestId");
+		const lastNameInput = screen.getByTestId("lastNameTestId");
+		const dateInput = screen.getByTestId("dateTestId");
+		const userTypeInput = screen.getByTestId("userTypeTestId");
+		const cpfInput = screen.getByTestId("cpfTestId");
+
 		const form = screen.getByTestId("formTestId");
 		userEvent.type(userEmail, "suellendavinci@gmail.com");
 		userEvent.type(passwordInput, "123456");
+		userEvent.type(firstNameInput, "suellen");
+		userEvent.type(lastNameInput, "camargo");
+		userEvent.type(dateInput, 958532400000);
+		userEvent.type(userTypeInput, "patient");
+		userEvent.type(cpfInput, "12545378996");
 		fireEvent.submit(form);
 		expect(mockedHandleSubmit).toHaveBeenCalled();
 	});
