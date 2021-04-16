3a51fedaab2b7e631f0a0cf6f0cda4da68d47e02
Graziela <grazi.m.kranz@gmail.com>
2021-04-16 00:58:39 -0300

    style(src/components/homePatient): initial styled

--- src/components/homePatient/index.jsx
+++ src/components/homePatient/index.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import UserBasicInfo from "../userBasicInfo";
-import { HomePatientContainer } from "./style";
+import {
+	HomePatientContainer,
+	ActionsContainer,
+	BtnContainer,
+	StyledBtn,
+} from "./style";
 
 const HomePatient = () => {
 	return (
 		<>
 			<HomePatientContainer>
 				<UserBasicInfo />
+				<ActionsContainer>
+					<BtnContainer>
+						<StyledBtn>Meus Lembretes</StyledBtn>
+					</BtnContainer>
+
+					<BtnContainer>
+						<StyledBtn>Ver todos médicos</StyledBtn>
+					</BtnContainer>
+
+					<BtnContainer>
+						<StyledBtn>Meu histórico</StyledBtn>
+					</BtnContainer>
+				</ActionsContainer>
 			</HomePatientContainer>
 		</>
 	);
--- src/components/homePatient/style.js
+++ src/components/homePatient/style.js
@@ -1,11 +1,62 @@
 import styled from "styled-components";
 
 export const HomePatientContainer = styled.div`
+	max-width: 650px;
 	@media (min-width: 1000px) {
 		display: flex;
 		flex-direction: row;
 		justify-content: flex-start;
 		padding: 5%;
 		overflow-x: hidden;
+	}
+`;
+
+export const ActionsContainer = styled.div`
+	flex-grow: 2;
+	margin: 3%;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-evenly;
+	@media (min-width: 1000px) {
+		min-height: 85vh;
+		margin: 0;
+		border: 2px solid black;
+		justify-content: flex-start;
+		align-items: center;
+	}
+`;
+export const BtnContainer = styled.div`
+	border: 3px solid rgba(114, 217, 224, 1);
+	height: 20%;
+	border-radius: 40px;
+	background-color: rgba(114, 217, 224, 0.2);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin-top: 5%;
+	@media (min-width: 1000px) {
+		height: 40%;
+		min-width: 80%;
+		max-width: 90%;
+		margin: 0 0 5% 0;
+	}
+`;
+
+export const StyledBtn = styled.button`
+	margin: 1rem 0;
+	width: 60%;
+	min-width: 150px;
+	height: 5rem;
+	border-radius: 9px;
+	border: none;
+	outline: none;
+	font-size: 1.5rem;
+	background-color: rgba(114, 217, 224, 1);
+	&:hover {
+		background-color: rgba(114, 217, 224, 0.7);
+	}
+	@media (min-width: 1000px) {
+		height: 7rem;
+		font-size: 3rem;
 	}
 `;
