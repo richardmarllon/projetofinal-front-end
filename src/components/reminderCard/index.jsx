494965f33b11b4425f3034e2f519c10fff7f382c
Franklin Percicotte <frankpercicotte@gmail.com>
2021-04-17 00:01:25 -0300

    fix(src/components/reminder..): update get inf

--- src/components/reminderCard/index.jsx
+++ src/components/reminderCard/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { CardContainer, Info, InfoContainer, Title } from "./style";
+import moment from "moment";
 
 const ReminderCard = ({ exam }) => {
 	return (
 		<>
 			<CardContainer>
-				<Title>consulta</Title>
+				<Title>Exame</Title>
 				<InfoContainer>
 					<Info>especialidade:</Info>
 					<Info>{exam.physicianSpecialty}</Info>
@@ -15,8 +16,12 @@ const ReminderCard = ({ exam }) => {
 					<Info>{exam.physicianName}</Info>
 				</InfoContainer>
 				<InfoContainer>
+					<Info>Descrição do Exame:</Info>
+					<Info>{exam.description}</Info>
+				</InfoContainer>
+				<InfoContainer>
 					<Info>data:</Info>
-					<Info>{exam.date}</Info>
+					<Info>{moment(exam.date).format('DD/MM/YYYY')}</Info>
 				</InfoContainer>
 			</CardContainer>
 		</>
--- src/components/reminderSection/index.jsx
+++ src/components/reminderSection/index.jsx
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useExams } from "../../providers/ExamsProvider";
 import ReminderCard from "../reminderCard";
-import { ReminderContainer } from "./style";
+import { ReminderContainer, CardSearch, Button } from "./style";
 import moment from "moment";
 
-const ReminderSection = () => {
-	const { userExams } = useExams();
-	const today = moment();
 
+
+const ReminderSection = () => {
+	const { userExams,loadApi } = useExams();
+	
+	const today = moment().format("YYYY/MM/DD");
+	console.log('today',today);
+	console.log('exams',userExams)
+
+	useEffect(() =>{
+
+		loadApi();
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	},[])
+
+	// moment('2010-10-20').isAfter('2010-01-01', 'year');
 	const futureExams = userExams.filter((exam) => {
-		return moment(exam.date).isAfter(today);
+		let examsDate = moment(Number(exam.date)).format("YYYY/MM/DD");
+		console.log('reminder time',moment(examsDate).isAfter(today,'year'))
+		return moment(examsDate).isAfter(today);
 	});
+
+	console.log('futureExams', futureExams)
 
 	return (
 		<>
 			<ReminderContainer>
 				{futureExams.map((exam) => {
-					return <ReminderCard key={exam.id} exam={exam} />;
+					return (
+						<CardSearch>
+							<ReminderCard key={exam.id} exam={exam} />
+						</CardSearch>
+					)
 				})}
+				<Button> Mais Inf. </Button>
 			</ReminderContainer>
 		</>
 	);
